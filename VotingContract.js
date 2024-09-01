6e23aa8ccc8d9f6866edd3fc6ee903ebf46d292d
Anmol <102401768+anmoldvh@users.noreply.github.com>
2024-09-01 18:18:22 +0530

    Delete VotingContract.js

--- VotingContract.js
+++
@@ -1,9 +0,0 @@
-import Web3 from 'web3';
-
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-const contractABI = [ /* ABI array from compilation */ ];
-const contractAddress = '0xYourContractAddress';
-
-const votingContract = new web3.eth.Contract(contractABI, contractAddress);
-
-export default votingContract;
